2cd610c8d131036df1e21e3f6d9d73854c910e62
vithar <roman.anokhin@combridge-itc.de>
2022-04-27 12:28:54 +0300

    [flutter] add registration/login ui

--- src/main/resources/mobileapp/chat/lib/main.dart
+++ src/main/resources/mobileapp/chat/lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:chat/pages/homePage.dart';
+import 'package:chat/pages/loginPage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+
+ColorScheme defaultColorScheme = const ColorScheme(
+  primary: Color(0xffBB86FC),
+  secondary: Color(0xff03DAC6),
+  surface: Color(0xff181818),
+  background: Color(0xff121212),
+  error: Color(0xffCF6679),
+  onPrimary: Color(0xff000000),
+  onSecondary: Color(0xff000000),
+  onSurface: Color(0xffffffff),
+  onBackground: Color(0xffffffff),
+  onError: Color(0xff000000),
+  brightness: Brightness.dark,
+);
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -14,10 +31,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: const LoginPage(title: 'Login UI'),
+      // home: HomePage(),
     );
   }
 }
